76207c1c80bba69eb3b41176094b128de550a090
David <david@renegadeprint.com>
2019-11-03 14:55:59 +1000

    cors

--- themes/fre/static/js/fresh.js
+++ themes/fre/static/js/fresh.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
       type: "POST",
       data: JSON.stringify(formData),
       contentType: "application/json",
+      crossDomain: true,
       url: "https://renegadeprint.com/cors/ajax",
       success: function(data) {
         // console.log(JSON.stringify(data))
